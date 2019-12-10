4dc6c5d9f32fe4f85b11a4eada3966eb2b2e1bd5
alactic <bluecard1992@yahoo.com>
2019-12-10 20:04:51 +0100

    adding mutual exclusion when sharing or writing to variable

--- main.go
+++ main.go
@@ -10,13 +10,16 @@ func main() {
 	arr := [7]string{"This", "is", "the", "tutorial", "of", "Go", "language"}
 	//  ch := make(chan struct{})
 	var wg sync.WaitGroup // number of working goroutines
+	var mu sync.Mutex     // guards icons
 	for _, f := range arr {
+		mu.Lock()
 		wg.Add(1)
 		go func(f string) {
 			defer wg.Done()
 			fmt.Println("array item :: ", f)
 			// ch <- struct{}{}
 		}(f)
+		mu.Unlock()
 	}
 	for range arr {
 		wg.Wait()
